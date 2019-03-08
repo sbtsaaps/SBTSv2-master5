70de3cd40ac29c889014a00d93b3cd14c6b4be8a
sbtsaaps <aapssbts@gmail.com>
2019-03-08 13:05:52 +0530

    parent form

--- app/src/main/java/sbts/dmw/com/sbtrackingsystem/activities/RegisterUserActivity.java
+++ app/src/main/java/sbts/dmw/com/sbtrackingsystem/activities/RegisterUserActivity.java
@@ -6,13 +6,16 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -26,6 +29,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Future;
 
 import sbts.dmw.com.sbtrackingsystem.R;
 import sbts.dmw.com.sbtrackingsystem.classes.SingletonClass;
@@ -34,20 +38,35 @@ import sbts.dmw.com.sbtrackingsystem.classes.SingletonClass;
 public class RegisterUserActivity extends AppCompatActivity {
     EditText full_name, dob, email, mobile1, mobile2, address, city, pincode, id, name;
 
-    RadioGroup gender;
+    RadioGroup radioGroup;
     Button photo;
-
-    String User;
+RadioButton gender;
+    String User, getUser;
     Bitmap bitmap;
-
+TextInputLayout textInputLayout;
+    int selectedId;
     final int PICK_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+        full_name = findViewById(R.id.register_full_name);
+        textInputLayout=findViewById(R.id.register_full_name_layout);
+        dob = findViewById(R.id.register_dob);
+        email = findViewById(R.id.register_email);
+        mobile1 = findViewById(R.id.register_Mobile1);
+        mobile2 = findViewById(R.id.register_Mobile2);
+        address = findViewById(R.id.register_address);
+        city = findViewById(R.id.register_city);
+        pincode = findViewById(R.id.register_pincode);
+        id=findViewById(R.id.register_student_id);
+        name = findViewById(R.id.register_student_name);
+        radioGroup = findViewById(R.id.radioGrp);
+        selectedId = radioGroup.getCheckedRadioButtonId();
+        gender=findViewById(selectedId);
+        photo = findViewById(R.id.register_photo);
 
-        photo=findViewById(R.id.register_photo);
         assert getSupportActionBar() != null;
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -123,4 +142,51 @@ public class RegisterUserActivity extends AppCompatActivity {
         SingletonClass.getInstance(getApplicationContext()).addToRequestQueue(imagerequest);
     }
 
+    public void uploaddata(View view) {
+
+
+        if (full_name.getText().toString().isEmpty()){
+
+            textInputLayout.setError("Enter Full Name");
+        }else{
+            textInputLayout.
+        }
+            if(dob.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Enter Date of Birth",Toast.LENGTH_LONG).show();
+
+
+        }else if (email.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Enter Email",Toast.LENGTH_LONG).show();
+
+
+        }else  if (mobile1.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Enter Mobile no",Toast.LENGTH_LONG).show();
+
+
+        }else  if (address.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Enter Address",Toast.LENGTH_LONG).show();
+
+
+        }else  if (city.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Enter City",Toast.LENGTH_LONG).show();
+
+
+        }else  if (pincode.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Enter Pincode",Toast.LENGTH_LONG).show();
+
+
+        }else  if (name.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Enter Student Name",Toast.LENGTH_LONG).show();
+
+
+        }else  if (gender.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(),"Select Gender",Toast.LENGTH_LONG).show();
+
+        }else if (id.getText().toString().isEmpty()){
+
+            Toast.makeText(getApplicationContext(),"Enter Student Id",Toast.LENGTH_LONG).show();
+        }
+
+
+    }
 }
