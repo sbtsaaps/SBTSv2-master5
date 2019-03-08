b0db64b31273849d7cc869da1b2e873407497035
sbtsaaps <aapssbts@gmail.com>
2019-03-08 11:02:40 +0530

    parent form

--- app/src/main/java/sbts/dmw/com/sbtrackingsystem/activities/RegisterUserActivity.java
+++ app/src/main/java/sbts/dmw/com/sbtrackingsystem/activities/RegisterUserActivity.java
@@ -3,10 +3,15 @@ package sbts.dmw.com.sbtrackingsystem.activities;
 import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import sbts.dmw.com.sbtrackingsystem.R;
 
+
+
 public class RegisterUserActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
